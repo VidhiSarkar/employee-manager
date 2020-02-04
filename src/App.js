6f39acd39a501e982ad1f8dcc88b277df1ea5f13
Amit Upadhyay <amitupadhyay10287@gmail.com>
2020-02-04 23:47:29 +0800

    Added configuration for react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import Employees from './containers/Employees';
+
+const routes = (
+    <Switch>
+        <Route path="/" exact component={Employees} />
+        <Redirect to="/" />
+    </Switch>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Header>
+            {routes}
+          </Header>
+      </div>
   );
 }
 
-export default App;
+export default withRouter(App);
+
